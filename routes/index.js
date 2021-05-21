6b721fa83d3bdb1fa048a8319cdbcc8eb8d413cb
niftykhee <koladelawal20@gmail.com>
2021-05-21 13:28:14 +0100

    changed script start

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+const JSONdb = require("simple-json-db");
+const db = new JSONdb("./db.json");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
